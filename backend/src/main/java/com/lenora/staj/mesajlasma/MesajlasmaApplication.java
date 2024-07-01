99e36373a012f4eda664a383942e716f28440974
iclluyrr <132147987+iclluyrr@users.noreply.github.com>
2024-07-01 14:05:27 +0300

    spring

---
+++ backend/src/main/java/com/lenora/staj/mesajlasma/MesajlasmaApplication.java
@@ -0,0 +1,13 @@
+package com.lenora.staj.mesajlasma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesajlasmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesajlasmaApplication.class, args);
+	}
+
+}
